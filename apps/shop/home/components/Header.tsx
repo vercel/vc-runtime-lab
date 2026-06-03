a423ba28ad3706de4d8c806ce5edea642a763030
tim123abc <18235985+tim123abc@users.noreply.github.com>
2026-06-03 09:59:22 +0100

    update cache life

--- apps/shop/home/components/Header.tsx
+++ apps/shop/home/components/Header.tsx
@@ -1,6 +1,8 @@
+import { cacheLife } from 'next/cache';
 import { ConsumeRemoteComponent } from 'remote-components/host/nextjs/app'
 
 export async function Header() {
-  'use cache'
+  "use cache";
+  cacheLife("days");
   return <ConsumeRemoteComponent isolate={false} src="/components/header" />
 }
